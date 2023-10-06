70eff6e61fa9f3d12ae4ac0b39c00cb3df7802f3
Anagha Yawale <111850322+anaghayawale@users.noreply.github.com>
2023-10-06 08:16:21 +0530

    Updated Signup Screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -21,77 +21,100 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Text(
-                  'Taskmaster',
-                  style: TextStyle(
-                    color: Constants().pinkColor(),
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0.0,
+                right: 0.0,
+                child: Image.asset(
+                  'assets/images/cornerImage.png',
+                  width: 350.0,
                 ),
-                Text(
-                  'Your personal task manager',
-                  style: TextStyle(
-                    color: Constants().blueColor(),
-                    fontSize: 20.0,
-                  ),
-                ),
-                const SizedBox(height: 25.0),
-                CustomTextField(hintText: 'Name', controller: nameController),
-                const SizedBox(height: 10.0),
-                CustomTextField(hintText: 'Email', controller: emailController),
-                const SizedBox(height: 10.0),
-                CustomTextField(
-                    hintText: 'Password', controller: passwordController),
-                const SizedBox(height: 10.0),
-                CustomTextField(
-                    hintText: 'Confirm Password',
-                    controller: confirmPasswordController),
-                const SizedBox(height: 10.0),
-                CustomButton(
-                  buttonName: 'Sign up',
-                  buttonColor: Constants().kLoginButtonColor(),
-                  buttonTextColor: Constants().kLoginButtonTextColor(),
-                  onPressed: () {},
-                ),
-                const SizedBox(height: 10.0),
-                Text(
-                  'OR',
-                  style:
-                      TextStyle(color: Constants().greyColor(), fontSize: 15.0),
-                ),
-                const SizedBox(height: 10.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already have an account? ',
-                      style: TextStyle(
-                        color: Constants().blueColor(),
-                        fontSize: 20.0,
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        'Login',
+              ),
+              Positioned(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 50.0),
+                      Text(
+                        'Taskmaster',
                         style: TextStyle(
                           color: Constants().pinkColor(),
-                          fontSize: 20.0,
+                          fontSize: 30.0,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                  ],
+                      Text(
+                        'Your personal task manager',
+                        style: TextStyle(
+                          color: Constants().blueColor(),
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      const SizedBox(height: 25.0),
+                      CustomTextField(
+                          hintText: 'Name', controller: nameController),
+                      const SizedBox(height: 10.0),
+                      CustomTextField(
+                          hintText: 'Email', controller: emailController),
+                      const SizedBox(height: 10.0),
+                      CustomTextField(
+                          hintText: 'Password', controller: passwordController),
+                      const SizedBox(height: 10.0),
+                      CustomTextField(
+                          hintText: 'Confirm Password',
+                          controller: confirmPasswordController),
+                      const SizedBox(height: 10.0),
+                      CustomButton(
+                        buttonName: 'Sign up',
+                        buttonColor: Constants().kLoginButtonColor(),
+                        buttonTextColor: Constants().kLoginButtonTextColor(),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'OR',
+                            style: TextStyle(
+                                color: Constants().greyColor(), fontSize: 15.0),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Already have an account? ',
+                            style: TextStyle(
+                              color: Constants().blueColor(),
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Constants().pinkColor(),
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
